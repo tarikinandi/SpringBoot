af14a8caa2cd6f4b486712e3878440fa7d2c4ae8
Tarik Inandi <muhtark02@hotmail.com>
2025-04-23 01:43:52 +0300

    Junit test added

--- exception-management/src/test/java/com/tarikinandi/exceptionmanagement/ExceptionManagementApplicationTests.java
+++ exception-management/src/test/java/com/tarikinandi/exceptionmanagement/ExceptionManagementApplicationTests.java
@@ -1,13 +1,38 @@
 package com.tarikinandi.exceptionmanagement;
 
+import com.tarikinandi.dto.EmployeeDTO;
+import com.tarikinandi.service.IEmployeeService;
+import com.tarikinandi.starter.ExceptionManagementApplication;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest(classes = {ExceptionManagementApplication.class})
 class ExceptionManagementApplicationTests {
 
+    @Autowired
+    private IEmployeeService employeeService;
+
+    @BeforeEach
+    public void BeforeEach(){
+        System.out.println("Before each çalıştı");
+    }
+
+    @AfterEach
+    public void AfterEach(){
+        System.out.println("After each çalıştı");
+    }
+
     @Test
-    void contextLoads() {
+    public void testFindEmployeeById() {
+        EmployeeDTO employee = employeeService.findEmployeeById(3L);
+        assertEquals(3L, employee.getId());
+        assertNotNull(employee.getDepartment());
     }
 
 }
