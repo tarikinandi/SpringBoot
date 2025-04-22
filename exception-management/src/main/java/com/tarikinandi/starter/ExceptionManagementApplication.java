5b8759bbd483f0805da5b5ee0fdc755e7a6567dd
Tarik Inandi <muhtark02@hotmail.com>
2025-04-23 01:15:33 +0300

    Scheduled example added

--- exception-management/src/main/java/com/tarikinandi/starter/ExceptionManagementApplication.java
+++ exception-management/src/main/java/com/tarikinandi/starter/ExceptionManagementApplication.java
@@ -5,10 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @EntityScan(basePackages = {"com.tarikinandi"})
 @EnableJpaRepositories(basePackages = "com.tarikinandi")
 @ComponentScan(basePackages = {"com.tarikinandi"})
+@EnableScheduling
 @SpringBootApplication
 public class ExceptionManagementApplication {
 
